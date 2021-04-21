8c0f889e8cb4eefe98c28797ed7e8b9e3cf2394b
mhdbagas <12050112788@students.uin-suska.ac.id>
2021-04-21 21:45:57 +0700

    algoritmabagas

---
+++ src/ALGORITMA/NimRun.java
@@ -0,0 +1,22 @@
+package ALGORITMA;
+
+import java.util.Scanner;
+
+public class NimRun {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nama: ");
+        String nama = input.nextLine();
+        System.out.print("NIM : ");
+        String nim = input.nextLine();
+
+        NIM objNim = new NIM();
+        objNim.setNim(nim);
+        System.out.println("Jenjang Perndidikan = "+objNim.getJenjang());
+        System.out.println("Tahun Masuk = 20"+objNim.getTahun());
+        System.out.println("Fakultas = "+objNim.getFakultas());
+        System.out.println("Jurusan = "+objNim.getJurusan());
+        System.out.println("Jenis kelamin = "+objNim.getJenisKelamin());
+        System.out.println("Nomor Urut Mahasiswa = "+objNim.getNomorurut());
+    }
+}
